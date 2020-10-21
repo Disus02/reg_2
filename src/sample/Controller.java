7ff250923980163b5315645d9debbfb300ac5691
Danil Stozhkov <lekseih2000@mail.ru>
2020-10-21 09:32:20 +0300

    Reg

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -2,21 +2,13 @@ package sample;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
-
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-
-
 public class Controller {
-    Module module = new Module();
 
+    Module module = new Module();
     @FXML
     private TextField name;
     @FXML
@@ -24,24 +16,13 @@ public class Controller {
     @FXML
     private Label status;
 
-
     @FXML
     public void pressRegistr(ActionEvent event) throws IOException {
-        module.createFile(name.getText(),password.getText());
-        status.setText("Вы зарегистрированы");
-        status.getText();
-
+        module.createFile(name,password,status);
     }
     @FXML
     public void PressOpen(ActionEvent event) throws IOException {
-        FileReader fis = new FileReader("C://test/Reg.txt");
-        String str;
-        var buffer=new BufferedReader(fis);
-        ArrayList<String> arrayList = new ArrayList<>();
-        while ((str = buffer.readLine())!=null){
-            if (str.isEmpty())break;
-            arrayList.add(str);
-        }
-
+        module.egualsFile(name,password,status);
     }
 }
+
--- src/sample/Module.java
+++ src/sample/Module.java
@@ -1,16 +1,46 @@
 package sample;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.*;
+import java.util.ArrayList;
 
 public class Module {
-    public void createFile(String login,String password) throws IOException {
+    public void createFile(TextField login,PasswordField password,Label status) throws IOException {
         File file = new File("C://test/Reg.txt");
         FileWriter fos = new FileWriter(file);
         file.createNewFile();
-        fos.write("Имя: " + login +"\n"+"Пароль: " + password);
+        if (login.getText().isEmpty()&&password.getText().isEmpty()){
+            status.setText("Введите логин и пароль: ");
+        }else if (login.getText().isEmpty()){
+            status.setText("Введите логин: ");
+        }else if (password.getText().isEmpty()){
+            status.setText("Ввведите пароль: ");
+        }else if (!login.getText().isEmpty()&&!password.getText().isEmpty()){
+            status.setText("Вы зарегистрированы ");
+        }
+        fos.write( login.getText() +"\n" + password.getText());
         fos.close();
     }
-
+    public void egualsFile(TextField name, PasswordField password, Label status) throws IOException {
+        FileReader fis = new FileReader("C://test/Reg.txt");
+        String str;
+        var buffer=new BufferedReader(fis);
+        ArrayList<String> arrayList = new ArrayList<>();
+        while ((str = buffer.readLine())!=null){
+            if (str.isEmpty())break;
+            arrayList.add(str);
+        }
+        ArrayList<String> arrayList1 = new ArrayList<>();
+        arrayList1.add(name.getText());
+        arrayList1.add(password.getText());
+        if (arrayList.equals(arrayList1)){
+            status.setText("Вы авторизованы");
+        }else status.setText("Зарегистрируйтесь: ");
+        fis.close();
+    }
 }
+
+
